626592d6b2ddcf003464ee8c6543fb08b34fc1cd
developer <developer@stargan.id>
2025-03-22 15:23:10 +0100

    ganti theme dengan accent biru

--- src/app/(beranda)/page.tsx
+++ src/app/(beranda)/page.tsx
@@ -1,7 +1,16 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
 export default function Beranda() {
   return (
-    <div>
-      <h1>Ini Halaman Beranda</h1>
+    <div className="flex flex-col items-center justify-center h-screen">
+      <div>
+        <Link href="/buat-akun-baru">
+          <Button size={"lg"} className="hover:cursor-pointer">
+            Buat Akun Sekarang
+          </Button>
+        </Link>
+      </div>
     </div>
   );
 }
